d6b86d264f23c88e0f586e7893b4386d72d6090b
Roberto Cardillo <roberto.cardillo000@gmail.com>
2024-05-15 16:27:48 +0000

    CREATING ORDERS OPERATIONAL

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,14 +1,24 @@
-select
-margin.orders_id
-,margin.date_date
-,margin.revenue
-,margin.quantity
-,margin.purchase_cost
-,margin.margin
-,ship.shipping_fee
-,ship.logcost
-,ship.ship_cost
- from {{ref("int_orders_margin")}} margin
-join {{ref("stg_raw__ship")}} ship using (orders_id)
-ORDER BY orders_id desc
+WITH orders AS (
+  SELECT
+    CAST(o.orders_id AS INT64) AS orders_id,
+    CAST(o.date_date AS DATE) AS date_date,
+    ROUND(
+      CAST(o.margin AS FLOAT64) +
+      CAST(s.shipping_fee AS FLOAT64) -
+      (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)),
+      2
+    ) AS operational_margin,
+    CAST(o.quantity AS INT64) AS quantity,
+    CAST(o.revenue AS FLOAT64) AS revenue,
+    CAST(o.purchase_cost AS FLOAT64) AS purchase_cost,
+    CAST(o.margin AS FLOAT64) AS margin,
+    CAST(s.shipping_fee AS FLOAT64) AS shipping_fee,
+    CAST(s.logcost AS FLOAT64) AS logcost,
+    CAST(s.ship_cost AS FLOAT64) AS ship_cost
+  FROM {{ ref('int_orders_margin') }} o
+  LEFT JOIN {{ ref('stg_raw__ship') }} s USING(orders_id)
+)
+SELECT * FROM orders
+ORDER BY orders_id DESC
+
 
